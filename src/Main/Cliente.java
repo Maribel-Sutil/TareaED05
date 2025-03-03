8984ee0b481d46c7ced0c09b13b6cfcf8812426d
María Isabel Sutil Cánovas <2776586@alu.murciaeduca.es>
2025-03-03 04:30:02 +0100

    Refactorización del archivo Cliente.java

--- src/Main/Cliente.java
+++ src/Main/Cliente.java
@@ -1,11 +1,41 @@
 public class Cliente {
-    public boolean esVip;
-    public String nombre;
-    public int id;
+    /**
+     * Paso de método publico a privado
+     */
+    boolean esVip;
+    private String nombre;
+    private int id;
 
-    public Cliente(boolean vip, String nombre, int id) {
-        this.esVip = vip;
+    /**
+     * Cambio el valor vip por esVip para mantener el código más homogéneo
+     */
+
+    public Cliente(boolean esVip, String nombre, int id) {
+        this.esVip = esVip;
         this.nombre = nombre;
+        this.id = id;
+    }
+    /**
+     *  Realizo los encapsulamientos, en el caso del boolean esVip me aconseja usar is en vez de get
+     *  ya que por convención es "más correcto".
+     */
+
+    public boolean isEsVip() {
+        return esVip;
+    }
+    public void setEsVip(boolean esVip) {
+        this.esVip = esVip;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
         this.id = id;
     }
 }

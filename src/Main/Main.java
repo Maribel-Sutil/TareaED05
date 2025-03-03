7b6ae7f910200691477e4bb78a1c1b8f8f86af89
María Isabel Sutil Cánovas <2776586@alu.murciaeduca.es>
2025-03-03 06:13:24 +0100

    Refactorización del archivo Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,16 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        /**
+         * Al refactorizar los otros dos archivos genera errores en este para solucionarlo habría que refactorizarlo tambien
+         */
         // Crea un pedido para un cliente normal
         Pedido pedido1 = new Pedido(new Cliente(false, "Juan", 1));
-        pedido1.procesarPedido(101);
+        pedido1.procesarPedido(101, false); // Actualiza para usar el método unificado
 
         // Crea un pedido para un cliente VIP
         Pedido pedido2 = new Pedido(new Cliente(true, "Juan de todos los santos", 2));
-        pedido2.procesarPedidoVIP(101);
+        pedido2.procesarPedido(101, true); // Actualiza para usar el método unificado
 
         // Convierte al cliente 1 en VIP y procesa un nuevo pedido de 1000 €
-        pedido1.cliente.esVip = true;
-        pedido1.procesarPedidoVIP(1000);
-
+        pedido1.getCliente().setEsVip(true);
+        pedido1.procesarPedido(1000, true); // Actualiza para usar el método unificado
     }
+
 }
